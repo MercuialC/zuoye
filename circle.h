9d2d8ade3c2db2606d535dc43e009b4541dc99db
MercuialC <946196959@qq.com>
2017-04-26 22:10:14 +0800

    circle.h
    .h

---
+++ circle.h
@@ -0,0 +1,14 @@
+//#include<iostream>
+//using namespace std;
+
+class Circle
+{
+public :
+	Circle();//定义一个圆的类
+	void input();//声明函数input
+	void output();//声明函数output
+private:
+	double _r;//定义圆的半径
+	double _s;//定义圆的面积
+	const double pi;//定义常量圆周率
+};
